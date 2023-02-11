50f773dd5d05813a09c1511275cc2a0696cb498e
Ahmed Kassem <ahmedkassem996@gmail.com>
2023-02-11 14:49:07 +0200

    changed color to purple

--- ProjectGithub/ContentView.swift
+++ ProjectGithub/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
         NavigationView {
             NavigationLink(destination: ModalView()){
                 Text("Go Next")
-            }.foregroundColor(.red)
+            }.foregroundColor(.purple)
             .navigationBarTitle("Xcode and git")
         }
     }
